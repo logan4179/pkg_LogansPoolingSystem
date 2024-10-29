e0e911c0794e1c4da000f07fd8fd8bb6f6631050
logan4179 <logan4179@hotmail.com>
2024-10-29 13:11:20 -0400

    Update Pool.cs

--- Pool.cs
+++ Pool.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//NOTE: older objects are NOT currently deactivated, but rather get cycled to the newest position in the world.
 namespace LogansPoolingSystem
 {
     public class Pool : MonoBehaviour
@@ -16,6 +17,9 @@ namespace LogansPoolingSystem
         [SerializeField] protected int count_allowedActiveInScene;
 
         protected int index_lastMadeActive = -1;
+
+        [SerializeField] protected bool rotateRandom = false;
+		[SerializeField, Tooltip("Allows you to randomly 'jitter' the spawn position around the spawn point")] protected float randomSpawnOffset = 0f;
 
 		protected virtual void Awake()
 		{
@@ -36,30 +40,59 @@ namespace LogansPoolingSystem
         }
 
         /// <summary>
-        /// Main method for spawning/cycling a new pooled instance. NOTE: older objects are NOT currently deactivated, 
-		/// but rather get cycled to the newest position in the world.
+        /// Main method for spawning/cycling a new pooled instance at an exact locaton and rotation (ignores any ramdoms)
         /// </summary>
-        /// <param name="pos_passed"></param>
+        /// <param name="spawnPoint"></param>
         /// <param name="rot_passed"></param>
         /// <returns></returns>
-        public virtual GameObject CycleSpawnAtPosition( Vector3 pos_passed, Quaternion rot_passed )
+        public virtual GameObject CycleSpawnExact( Vector3 spawnPoint, Quaternion rot_passed )
         {
 			int nextPos = LPS_Utils.GetLoopedIndex( pooledObjects.Length, index_lastMadeActive + 1 );
 			GameObject go = pooledObjects[nextPos];
 
 			index_lastMadeActive = nextPos;
 
-            go.transform.position = pos_passed;
+            go.transform.position = spawnPoint;
             go.transform.rotation = rot_passed;
+
 			go.SetActive( true );
 
             return go;
         }
 
-        /// <summary>
-        /// Sets all pooled objects to inactive, disappearing them from the scene. Also resets pool spawn order.
-        /// </summary>
-        public void DeactivateMyObjects()
+		/// <summary>
+		/// This overload allows you to rotate the instance to look down vNormal.
+		/// </summary>
+		/// <param name="spawnPoint"></param>
+		/// <param name="vNormal"></param>
+		/// <returns></returns>
+		public virtual GameObject CycleSpawn( Vector3 spawnPoint, Vector3 vNormal )
+		{
+			int nextPos = LPS_Utils.GetLoopedIndex( pooledObjects.Length, index_lastMadeActive + 1 );
+			GameObject go = pooledObjects[nextPos];
+
+			index_lastMadeActive = nextPos;
+
+			go.transform.position = spawnPoint;
+
+			if ( rotateRandom )
+			{
+				go.transform.rotation = Quaternion.AngleAxis( Random.Range(0f, 360f), vNormal) * Quaternion.LookRotation(vNormal);
+			}
+			else
+			{
+				go.transform.rotation = Quaternion.LookRotation( vNormal );
+			}
+
+			go.SetActive( true );
+
+			return go;
+		}
+
+		/// <summary>
+		/// Sets all pooled objects to inactive, disappearing them from the scene. Also resets pool spawn order.
+		/// </summary>
+		public void DeactivateMyObjects()
         {
 			if ( pooledObjects != null && pooledObjects.Length > 0 )
 			{
